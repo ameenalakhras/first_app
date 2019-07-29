852c1d1d702887980563f2b2fb0f0f5c0b8010cf
aminalakhras <amin603123@gmail.com>
2019-07-29 23:13:44 +0300

    fix flake8 errors (make syntax looks better)

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-# from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
@@ -17,9 +13,11 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, last_name, birth_date, first_name=None,  password=None):
+    def create_superuser(self, email, last_name, birth_date,
+                         first_name=None,  password=None):
         superuser = self.create_user(email, first_name, last_name, birth_date,
-                                     password=password, is_active=True, is_coach=True, is_admin=True)
+                                     password=password, is_active=True,
+                                     is_coach=True, is_admin=True)
         return superuser
 
 
@@ -38,111 +36,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def is_superuser(self):
         return self.is_admin
-
-
-
-
-
-
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.EmailField(unique=True)
-#     phone_number = models.CharField(max_length=100)
-#     city_address = models.CharField(max_length=100)
-#     street_address = models.CharField(max_length=100)
-#     birth_date = models.DateField(null=True)
-#     is_active = models.BooleanField()
-#     is_coach = models.BooleanField()
-#     is_admin = models.BooleanField()
-#     join_date = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-
-
-
-
-
-
-
-#
-#
-#
-#
-#
-#
-# class MemberManager(BaseUserManager):
-#     def create_user(self, email, first_name, last_name, birth_date, password=None, is_active=False, is_coach=False, is_admin=False):
-#         if not email:
-#             raise ValueError("User must have Email address")
-#         if not password:
-#             raise ValueError("User must have Password")
-#         if not first_name:
-#             raise ValueError("User must have First name")
-#         if not last_name:
-#             raise ValueError("User must have Last name")
-#         if not birth_date:
-#             raise ValueError("User must have birth date")
-#
-#         user_obj = self.model(
-#             email=self.normalize_email(email)
-#         )
-#         user_obj.set_password(password)
-#         user_obj.first_name = first_name
-#         user_obj.last_name = last_name
-#         user_obj.birth_date = birth_date
-#         user_obj.is_active = is_active
-#         user_obj.is_admin = is_admin
-#         user_obj.is_coach = is_coach
-#         user_obj.save(using=self._db)
-#         return user_obj
-#
-#     def create_coach(self, email, first_name, last_name, birth_date, password=None):
-#         coach = self.create_user(email, first_name, last_name,
-#                                  birth_date, password=password, is_active=True, is_coach=True)
-#         return coach
-#
-#     def create_superuser(self, email, last_name, birth_date, first_name=None,  password=None):
-#         superuser = self.create_user(email, first_name, last_name, birth_date,
-#                                      password=password, is_active=True, is_coach=True, is_admin=True)
-#         return superuser
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# class Member(AbstractBaseUser, PermissionsMixin):
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.EmailField(unique=True)
-#     phone_number = models.CharField(max_length=100)
-#     city_address = models.CharField(max_length=100)
-#     street_address = models.CharField(max_length=100)
-#     birth_date = models.DateField(null=True)
-#     is_active = models.BooleanField()
-#     is_coach = models.BooleanField()
-#     is_admin = models.BooleanField()
-#     join_date = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name']
-#
-#     objects = MemberManager()
-#
-#     def __str__(self):
-#         return self.email
-#
-#     def get_full_name(self):
-#         return f"{self.first_name} {self.last_name}"
-#
-#     @property
-#     def is_staff(self):
-#         return True
-#
-#     @property
-#     def is_superuser(self):
-#         return self.is_admin
