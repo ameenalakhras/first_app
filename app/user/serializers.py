862099173d1b0a6833c2c461a029c56a5fe47314
aminalakhras <amin603123@gmail.com>
2019-08-02 17:45:13 +0300

    add manage(update) user apis

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as translate_language
 from rest_framework import serializers
 
 
-class userSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
 
     class Meta:
@@ -15,6 +15,19 @@ class userSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validate_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        # None is the default value if pop doesn't exist
+        password = validated_data.pop("password", None)
+        # super will call ModelSerializer update() function
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
--- app/user/views.py
+++ app/user/views.py
@@ -1,15 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from user.serializers import userSerializer, AuthTokenSerializer
+
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
-    serializer_class = userSerializer
+    serializer_class = UserSerializer
 
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    # it can be cookie authentication instead of token authentication
+    authentication_classes = (authentication.TokenAuthentication,)
+    # the level of access the user have
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
